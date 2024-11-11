6d0c0ab5ea69954926299fc1c47039acfbdd28b4
juliapiek <piekarskajulia337@interia.pl>
2024-11-11 19:15:05 +0100

    zadanie 4

---
+++ Szablonowanie+internacjonalizacja/src/main/java/com/jsfcourse/calc/CalcBB.java
@@ -0,0 +1,99 @@
+package com.jsfcourse.calc;
+
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+import java.util.Locale;
+
+@Named
+@RequestScoped
+//@SessionScoped
+public class CalcBB {
+	private Double kwota = null;
+	private Double oprocentowanie = null;
+        private Integer lata = null;
+	private Double result;
+        private String locale = "pl";
+
+	@Inject
+	FacesContext ctx;
+
+    public Double getKwota() {
+        return kwota;
+    }
+
+    public void setKwota(Double kwota) {
+        this.kwota = kwota;
+    }
+
+    public Double getOprocentowanie() {
+        return oprocentowanie;
+    }
+
+    public void setOprocentowanie(Double oprocentowanie) {
+        this.oprocentowanie = oprocentowanie;
+    }
+
+    public Integer getLata() {
+        return lata;
+    }
+
+    public void setLata(Integer lata) {
+        this.lata = lata;
+    }
+
+    public Double getResult() {
+        return result;
+    }
+
+    public void setResult(Double result) {
+        this.result = result;
+    }
+    
+
+
+
+	public boolean doTheMath() {
+		try {
+			
+
+			result = (kwota * (oprocentowanie/100/12)) / 
+                          (1 - Math.pow(1 + (oprocentowanie/100/12), -(lata*12)));
+                       
+
+			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Operacja wykonana poprawnie", null));
+			return true;
+		} catch (Exception e) {
+			ctx.addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "Błąd podczas przetwarzania parametrów", null));
+			return false;
+		}
+	}
+
+	// Go to "showresult" if ok
+	public String calc() {
+		if (doTheMath()) {
+			return "showresult";
+		}
+		return null;
+	}
+
+	// Put result in messages on AJAX call
+	public String calc_AJAX() {
+		if (doTheMath()) {
+			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Miesięczna rata: " + result, null));
+		}
+		return null;
+	}
+
+	public String info() {
+		return "info";
+	}
+        public void setLocale(String language) {
+        this.locale = language;
+        FacesContext.getCurrentInstance().getViewRoot().setLocale(new Locale(language));
+    }
+}
